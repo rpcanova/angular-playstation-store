7266aabe35a5e3c69d0ab1b8c98b5370256b08d6
unknown <rpcanova@hotmail.com>
2024-12-04 01:02:26 -0300

    initial commit

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+    @Input()
+    gameCover: string = ""
+
+    @Input()
+    gameLabel: string = ""
+
+    @Input()
+    gameType: string = "XPTO | PS4"
+
+    @Input()
+    gamePrice: string = "R$399,90"
+}
---
+++ src/app/components/navbar/navbar-item/navbar-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-item',
+  templateUrl: './navbar-item.component.html',
+  styleUrl: './navbar-item.component.css'
+})
+export class NavbarItemComponent {
+
+}
